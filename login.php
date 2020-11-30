d953a319730a6c75d73bfb2da5f1055cd3588e0b
sebastian <jaimetenorio_geografia@outlook.es>
2020-11-30 07:01:24 -0600

    Se cambio la funcion usada para validar el sql injection

--- login.php
+++ login.php
@@ -1,13 +1,14 @@
 <?php session_start();
     if($_SERVER['REQUEST_METHOD']=='POST'){
-        
-        $usuario = filter_var(strtolower($_POST['userName']),FILTER_SANITIZE_STRING);
-        $password = filter_var($_POST['Password'],FILTER_SANITIZE_STRING);
+
+        //evita el sql injection convirtiendo los caracteres especiales en entidades html
+        $user = htmlspecialchars(strtolower($_POST['userName']));
+        $password = htmlspecialchars(strtolower($_POST['Password']));
         $errores='';
     
         try {
             include 'db/db_conexion.php';
-            $query = "SELECT COUNT(*) as count FROM usuario where Usuario='$usuario' AND Password = '$password'";
+            $query = "SELECT COUNT(*) as count FROM usuario where Usuario='$user' AND Password = '$password'";
             $resultado = mysqli_query($conexion,$query);
             $array = mysqli_fetch_array($resultado);
             if ($array['count']>0) {
