1521622b6fee20c176c01a49d0d05983ec1fa655
Jorge Puente <cokinps20@gmail.com>
2020-12-04 21:19:43 -0600

    Commit para poder hacer push

--- js/cart2.js
+++ js/cart2.js
@@ -5,7 +5,7 @@ var fadeTime = 300;
 
 
 /* Assign actions */
-$('.quantity input').change(function() {
+$('.quantity').change(function() {
   updateQuantity(this);
 });
 
@@ -17,28 +17,6 @@ $(document).ready(function() {
   updateSumItems();
 });
 
-$('.promo-code-cta').click(function() {
-
-  promoCode = $('#promo-code').val();
-
-  if (promoCode == '10off' || promoCode == '10OFF') {
-    //If promoPrice has no value, set it as 10 for the 10OFF promocode
-    if (!promoPrice) {
-      promoPrice = 10;
-    } else if (promoCode) {
-      promoPrice = promoPrice * 1;
-    }
-  } else if (promoCode != '') {
-    alert("Invalid Promo Code");
-    promoPrice = 0;
-  }
-  //If there is a promoPrice that has been set (it means there is a valid promoCode input) show promo
-  if (promoPrice) {
-    $('.summary-promo').removeClass('hide');
-    $('.promo-value').text(promoPrice.toFixed(2));
-    recalculateCart(true);
-  }
-});
 
 /* Recalculate cart */
 function recalculateCart(onlyTotal) {
